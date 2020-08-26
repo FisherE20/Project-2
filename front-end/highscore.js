852267e7d3a959861a8f50d4e05dd697948323e5
DarkDave <darkdave1185@gmail.com>
2020-08-25 21:17:20 -0700

    added signup/login on testform.html links included

---
+++ front-end/highscore.js
@@ -0,0 +1,10 @@
+/*for (let i = 1; i <= 5000; i++) {
+    console.log("Script connected")
+    $(".container").append("<div class = 'box'></div>")
+};*/
+
+
+for (let i = 1; i <= 5000; i++) {
+    console.log("Script connected")
+    $(".container").append("<div class = box'></div>")
+}
