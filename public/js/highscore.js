d2c96d0a5f97f1e6c0fa4e99442b770c06cc30d8
DarkDave <darkdave1185@gmail.com>
2020-08-26 19:36:20 -0700

    added some more styling for last pages

--- public/js/highscore.js
+++ public/js/highscore.js
@@ -1,10 +1,10 @@
-/*for (let i = 1; i <= 5000; i++) {
+for (let i = 1; i <= 2500; i++) {
     console.log("Script connected")
     $(".container").append("<div class = 'box'></div>")
-};*/
+};
 
 
-for (let i = 1; i <= 5000; i++) {
+/*for (let i = 1; i <= 5000; i++) {
     console.log("Script connected")
     $(".container").append("<div class = box'></div>")
-}
+}*/
