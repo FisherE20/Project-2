c6c1f51133ad82cf11f8ae57e751c7ca6e8808a6
DarkDave <darkdave1185@gmail.com>
2020-08-27 19:59:40 -0700

    commit card changes and edit spacing

--- public/js/highscore.js
+++ public/js/highscore.js
@@ -1,10 +1,5 @@
+/*create division on page for color animation*/
 for (let i = 1; i <= 2500; i++) {
     console.log("Script connected")
     $(".container").append("<div class = 'box'></div>")
 };
-
-
-/*for (let i = 1; i <= 5000; i++) {
-    console.log("Script connected")
-    $(".container").append("<div class = box'></div>")
-}*/
